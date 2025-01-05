4f709a0b243031ea5483c9ecdbeeeae36f66115b
= <=>
2025-01-05 20:00:45 +0100

    part 2 - basic program layout

--- main.c
+++ main.c
@@ -7,6 +7,56 @@
 const float ScreenWidth = 1408.0f;
 const float ScreenHeight = 792.0f;
 
+const Clay_Color COLOR_LIGHT = (Clay_Color){224, 215, 210, 255};
+const Clay_Color COLOR_RED = (Clay_Color){168, 66, 28, 255};
+const Clay_Color COLOR_ORANGE = (Clay_Color){225, 138, 50, 255};
+const Clay_Color COLOR_WHITE = (Clay_Color){.r = 255, .g = 255, .b = 255};
+const Clay_Color COLOR_DARK = (Clay_Color){100, 100, 100, 255};
+
+Clay_RenderCommandArray BuildLayout(void)
+{
+  // Create layout
+  Clay_BeginLayout();
+
+  CLAY(CLAY_ID("Main"), CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}), CLAY_RECTANGLE({.color = COLOR_DARK}))
+  {
+    CLAY(CLAY_ID("Header"),
+         CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_FIXED(50)}, .childAlignment = {0, CLAY_ALIGN_Y_CENTER}, .childGap = 16, .padding = {32}}),
+         CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+    {
+    }
+
+    CLAY(CLAY_ID("CenterContainer"), CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, .childGap = 16}), CLAY_RECTANGLE({.color = COLOR_DARK}))
+    {
+
+      CLAY(CLAY_ID("SideBar1"),
+           CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_FIXED(300), .height = CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}),
+           CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+      {
+      }
+      CLAY(CLAY_ID("MainContent"),
+           CLAY_LAYOUT({.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_GROW()}}),
+           CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+      {
+      }
+
+      CLAY(CLAY_ID("SideBar2"),
+           CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_FIXED(300), .height = CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}),
+           CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+      {
+      }
+    }
+
+    CLAY(CLAY_ID("Footer"),
+         CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_FIXED(200)}, .childAlignment = {0, CLAY_ALIGN_Y_CENTER}, .childGap = 16, .padding = {32}}),
+         CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+    {
+    }
+  }
+
+  return Clay_EndLayout();
+}
+
 int main(void)
 {
   // Init raylib
@@ -22,6 +72,7 @@ int main(void)
       .width = ScreenWidth,
       .height = ScreenHeight,
   };
+  Clay_SetMeasureTextFunction(Raylib_MeasureText);
   Clay_Initialize(memoryArena, dimensions);
 
   // ...more init??
@@ -30,12 +81,32 @@ int main(void)
   // Main application loop
   while (!WindowShouldClose())
   {
+    float dt = GetFrameTime();
+
     // Logic code
+
+    // Update states
+    Vector2 raylibMousePosition = GetMousePosition();
+    Clay_Vector2 mousePosition = {
+        .x = raylibMousePosition.x,
+        .y = raylibMousePosition.y,
+    };
+    Clay_SetPointerState(mousePosition, IsMouseButtonDown(MOUSE_LEFT_BUTTON));
+
+    Vector2 raylibMouseWheelDelta = GetMouseWheelMoveV();
+    Clay_Vector2 mouseWheelDelta = {
+        .x = raylibMouseWheelDelta.x,
+        .y = raylibMouseWheelDelta.y,
+    };
+    Clay_UpdateScrollContainers(true, mouseWheelDelta, dt);
+
+    Clay_RenderCommandArray renderCommands = BuildLayout();
 
     // Render stuff
     BeginDrawing();
     ClearBackground(YELLOW);
     // TODO: render the Clay layout
+    Clay_Raylib_Render(renderCommands);
     EndDrawing();
   }
 
