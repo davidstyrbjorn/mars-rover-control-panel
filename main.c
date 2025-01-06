8e6f798005e737ff988c26723419fcb2f5f7d29d
davidstyrbjorn <davidstyrbjorn@gmail.com>
2025-01-06 22:40:15 +0100

    part 4 - buttons finally done!

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ const Clay_Color ColorButton = (Clay_Color){20, 20, 20, 255};
 const Clay_Color ColorButtonHover = (Clay_Color){40, 40, 40, 255};
 const Clay_Color ColorText = (Clay_Color){193, 68, 14, 255};
 const Clay_Color ColorBg = (Clay_Color){69, 24, 4, 255};
+const Clay_Color ColorBlack = (Clay_Color){0, 0, 0, 255};
 
 // =================================
 //            FONT IDS
@@ -45,6 +46,8 @@ static const char *PowerOnButtonID = "power-on-btn";
 static const char *PowerThrustersButtonID = "power-thrusters-btn";
 static const char *CollectSamplesButtonID = "collect-samples-btn";
 static const char *EmergencyShutdownButtonID = "emergency-shutdown-btn";
+static const char *InitControlledDescendButtonID = "init-controlled-descend-btn";
+static const char *InitManualControlButtonID = "init-manual-control-btn";
 
 // 16:9 resolution
 const float ScreenWidth = 1400.0f; // UPDATED
@@ -52,7 +55,10 @@ const float ScreenHeight = 850.0f; // UPDATED
 
 typedef void (*ButtonCallback)(void);
 
-Image testImage;
+Texture rightArrowTexture;
+Texture leftArrowTexture;
+Texture upArrowTexture;
+const float ArrowImageSize = 80;
 
 // =================================
 //          BUTTON CALLBACKS
@@ -93,7 +99,7 @@ void button_element(const char *id, Clay_String text, ButtonCallback callback)
 {
   CLAY(CLAY_ID(id),
        CLAY_LAYOUT(buttonLayoutConfig),
-       CLAY_RECTANGLE({.color = Clay_Hovered() ? ColorButtonHover : ColorButton}),
+       CLAY_RECTANGLE({.color = Clay_Hovered() ? ColorButtonHover : ColorButton, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}),
        Clay_OnHover(handle_button_interaction, (intptr_t)callback))
   {
     CLAY_TEXT(text, CLAY_TEXT_CONFIG(ButtonTextConfig));
@@ -122,8 +128,6 @@ Clay_RenderCommandArray build_layout(void)
       {
         // When id's are button_1, button_2, button_3, button_4, they all seem to get the same hash?
         // Should investigate the exact output of the clay hashing function
-        // assert(Clay__HashString(CLAY_STRING("button_1"), 0, 0) == Clay__HashString(CLAY_STRING("button_1"), 0, 0)); ??????
-
         button_element(PowerOnButtonID, CLAY_STRING("Power On"), power_on_button);
         button_element(PowerThrustersButtonID, CLAY_STRING("Power Thrusters"), power_thrusters_button);
         button_element(CollectSamplesButtonID, CLAY_STRING("Collect Samples"), collect_samples_button);
@@ -132,18 +136,38 @@ Clay_RenderCommandArray build_layout(void)
 
       CLAY(CLAY_ID("MainContent"),
            CLAY_LAYOUT({.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_GROW()}}),
-           CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+           CLAY_RECTANGLE({.color = ColorBlack, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
       {
       }
 
       CLAY(CLAY_ID("SideBar2"),
-           CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_FIXED(300), .height = CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}),
+           CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_FIXED(300), .height = CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16, .childAlignment = {.x = CLAY_ALIGN_X_CENTER, .y = 0}}),
            CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
       {
-        CLAY(CLAY_ID("SyntaxPageRightImage"), CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW()}, .childAlignment = {.x = CLAY_ALIGN_X_CENTER}}))
+        CLAY(CLAY_ID("UpArrowImage"),
+             CLAY_LAYOUT({.sizing = {.width = CLAY_SIZING_FIXED(90), .height = CLAY_SIZING_FIXED(90)}}),
+             CLAY_RECTANGLE({.color = Clay_Hovered() ? ColorButtonHover : ColorButton, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}),
+             CLAY_IMAGE({.sourceDimensions = {ArrowImageSize, ArrowImageSize}, .imageData = &upArrowTexture}))
         {
-          CLAY(CLAY_ID("SyntaxPageRightImageInner"), CLAY_IMAGE({.sourceDimensions = {360, 360}, .imageData = &testImage})) {}
         }
+        CLAY(CLAY_ID("ArrowBottomRow"),
+             CLAY_LAYOUT({.layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_FIXED(100)}, .childAlignment = {.x = CLAY_ALIGN_X_CENTER, .y = 0}, .childGap = 78}))
+        {
+          CLAY(CLAY_ID("LeftArrowImage"),
+               CLAY_LAYOUT({.sizing = {.width = CLAY_SIZING_FIXED(90), .height = CLAY_SIZING_FIXED(90)}}),
+               CLAY_RECTANGLE({.color = Clay_Hovered() ? ColorButtonHover : ColorButton, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}),
+               CLAY_IMAGE({.sourceDimensions = {ArrowImageSize, ArrowImageSize}, .imageData = &leftArrowTexture}))
+          {
+          }
+          CLAY(CLAY_ID("RightArrowImage"),
+               CLAY_LAYOUT({.sizing = {.width = CLAY_SIZING_FIXED(90), .height = CLAY_SIZING_FIXED(90)}}),
+               CLAY_RECTANGLE({.color = Clay_Hovered() ? ColorButtonHover : ColorButton, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}),
+               CLAY_IMAGE({.sourceDimensions = {ArrowImageSize, ArrowImageSize}, .imageData = &rightArrowTexture}))
+          {
+          }
+        }
+        button_element(InitControlledDescendButtonID, CLAY_STRING("Init Descend"), power_on_button);
+        button_element(InitManualControlButtonID, CLAY_STRING("Init Manual Control"), power_on_button);
       }
     }
 
@@ -168,6 +192,11 @@ void insert_font(uint32_t font_id, int font_size, const char *path)
   SetTextureFilter(Raylib_fonts[font_id].font.texture, TEXTURE_FILTER_TRILINEAR);
 }
 
+void handle_clay_error(Clay_ErrorData errorData)
+{
+  printf("%s", errorData.errorText.chars);
+}
+
 int main(void)
 {
   // Init raylib
@@ -184,7 +213,7 @@ int main(void)
       .height = ScreenHeight,
   };
   Clay_SetMeasureTextFunction(Raylib_MeasureText);
-  Clay_Initialize(memory_arena, dimensions);
+  Clay_Initialize(memory_arena, dimensions, (Clay_ErrorHandler){handle_clay_error});
 
   // ...more init??
   SetTargetFPS(144);
@@ -197,7 +226,9 @@ int main(void)
   insert_font(FontIdItalic24, 24, "resources/0xProto-Italic.ttf");
   insert_font(FontIdTitle, 36, "resources/0xProto-Regular.ttf");
 
-  testImage = LoadImage("resources/test.png");
+  rightArrowTexture = LoadTextureFromImage(LoadImage("resources/arrow-right.png"));
+  leftArrowTexture = LoadTextureFromImage(LoadImage("resources/arrow-left.png"));
+  upArrowTexture = LoadTextureFromImage(LoadImage("resources/arrow-up.png"));
 
   // Main application loop
   while (!WindowShouldClose())
@@ -223,7 +254,7 @@ int main(void)
 
     // Render stuff
     BeginDrawing();
-    ClearBackground(YELLOW);
+    ClearBackground(BLACK);
     // TODO: render the Clay layout
     Clay_Raylib_Render(renderCommands);
     EndDrawing();
