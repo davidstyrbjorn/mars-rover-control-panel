e603cb56da936b1aec15429a087c0561fc75c6d3
davidstyrbjorn <davidstyrbjorn@gmail.com>
2025-01-06 15:23:25 +0100

    part 4 - buttons 2, now clickable, almost done...

--- clay.h
+++ clay.h
@@ -14,6 +14,7 @@
 #include "stdint.h"
 #include "stdbool.h"
 #include "stddef.h"
+#include "stdio.h"
 
 // -----------------------------------------
 // HEADER DECLARATIONS ---------------------
--- main.c
+++ main.c
@@ -6,32 +6,30 @@
 // =================================
 //              COLORS
 // =================================
-const Clay_Color COLOR_LIGHT = (Clay_Color){224, 215, 210, 255};
-const Clay_Color COLOR_RED = (Clay_Color){168, 66, 28, 255};
-const Clay_Color COLOR_ORANGE = (Clay_Color){225, 138, 50, 255};
-const Clay_Color COLOR_WHITE = (Clay_Color){.r = 255, .g = 255, .b = 255};
-const Clay_Color COLOR_DARK = (Clay_Color){100, 100, 100, 255};
-const Clay_Color COLOR_BUTTON = (Clay_Color){20, 20, 20, 255};
-const Clay_Color COLOR_BUTTON_HOVER = (Clay_Color){40, 40, 40, 255};
+const Clay_Color ColorLight = (Clay_Color){240 * 0.8, 231 * 0.8, 231 * 0.8, 255};
+const Clay_Color ColorDark = (Clay_Color){50, 50, 50, 255};
+const Clay_Color ColorButton = (Clay_Color){20, 20, 20, 255};
+const Clay_Color ColorButtonHover = (Clay_Color){40, 40, 40, 255};
+const Clay_Color ColorText = (Clay_Color){193, 68, 14, 255};
+const Clay_Color ColorBg = (Clay_Color){69, 24, 4, 255};
 
 // =================================
 //            FONT IDS
 // =================================
-const uint32_t FONT_ID_NORMAL_16 = 0;
-const uint32_t FONT_ID_NORMAL_24 = 1;
-const uint32_t FONT_ID_BOLD_16 = 2;
-const uint32_t FONT_ID_BOLD_24 = 3;
-const uint32_t FONT_ID_ITALIC_16 = 4;
-const uint32_t FONT_ID_ITALIC_24 = 5;
-const uint32_t FONT_ID_TITLE = 6;
+const uint32_t FontIdNormal16 = 0;
+const uint32_t FontIdNormal24 = 1;
+const uint32_t FontIdBold16 = 2;
+const uint32_t FontIdBold24 = 3;
+const uint32_t FontIdItalic16 = 4;
+const uint32_t FontIdItalic24 = 5;
+const uint32_t FontIdTitle = 6;
 
 // =================================
 //      ELEMENT CONFIGURATIONS
 // =================================
-const Clay_TextElementConfig headerTextConfig = (Clay_TextElementConfig){.fontId = FONT_ID_TITLE, .fontSize = 36, .textColor = COLOR_DARK};
-const Clay_TextElementConfig footerTextConfig = (Clay_TextElementConfig){.fontId = FONT_ID_BOLD_16, .fontSize = 16, .textColor = COLOR_DARK};
-const Clay_TextElementConfig buttonTextConfig = (Clay_TextElementConfig){.fontId = FONT_ID_NORMAL_24, .fontSize = 24, .textColor = COLOR_LIGHT};
-
+const Clay_TextElementConfig HeaderTextConfig = (Clay_TextElementConfig){.fontId = FontIdTitle, .fontSize = 36, .textColor = ColorText};
+const Clay_TextElementConfig FooterTextConfig = (Clay_TextElementConfig){.fontId = FontIdBold24, .fontSize = 24, .textColor = ColorText};
+const Clay_TextElementConfig ButtonTextConfig = (Clay_TextElementConfig){.fontId = FontIdNormal24, .fontSize = 24, .textColor = ColorText};
 Clay_LayoutConfig buttonLayoutConfig = {
     .sizing = {
         .width = CLAY_SIZING_GROW(),
@@ -40,21 +38,65 @@ Clay_LayoutConfig buttonLayoutConfig = {
     .childAlignment = {.y = CLAY_ALIGN_Y_CENTER, .x = CLAY_ALIGN_X_CENTER},
 };
 
+// =================================
+//          ELEMENT IDs
+// =================================
+static const char *PowerOnButtonID = "power-on-btn";
+static const char *PowerThrustersButtonID = "power-thrusters-btn";
+static const char *CollectSamplesButtonID = "collect-samples-btn";
+static const char *EmergencyShutdownButtonID = "emergency-shutdown-btn";
+
 // 16:9 resolution
 const float ScreenWidth = 1400.0f; // UPDATED
 const float ScreenHeight = 850.0f; // UPDATED
 
-void handle_button_interaction(Clay_ElementId elementId, Clay_PointerData pointerInfo, intptr_t userData)
+typedef void (*ButtonCallback)(void);
+
+Image testImage;
+
+// =================================
+//          BUTTON CALLBACKS
+// =================================
+void power_on_button(void)
 {
+  printf("Power ON\n");
 }
 
-void button_element(const char *id, Clay_String text)
+void power_thrusters_button(void)
+{
+  printf("Power thrusters\n");
+}
+
+void collect_samples_button(void)
+{
+  printf("Collect samples\n");
+}
+
+void emergency_shutdown_button(void)
+{
+  printf("Emergency shutdown\n");
+}
+
+void handle_button_interaction(Clay_ElementId element_id, Clay_PointerData pointerInfo, intptr_t userData)
+{
+  if (pointerInfo.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME)
+  {
+    ButtonCallback cb = (ButtonCallback)userData;
+    if (cb)
+    {
+      cb();
+    }
+  }
+}
+
+void button_element(const char *id, Clay_String text, ButtonCallback callback)
 {
   CLAY(CLAY_ID(id),
        CLAY_LAYOUT(buttonLayoutConfig),
-       CLAY_RECTANGLE({.color = Clay_Hovered() ? COLOR_BUTTON_HOVER : COLOR_BUTTON}))
+       CLAY_RECTANGLE({.color = Clay_Hovered() ? ColorButtonHover : ColorButton}),
+       Clay_OnHover(handle_button_interaction, (intptr_t)callback))
   {
-    CLAY_TEXT(text, CLAY_TEXT_CONFIG(buttonTextConfig));
+    CLAY_TEXT(text, CLAY_TEXT_CONFIG(ButtonTextConfig));
   }
 }
 
@@ -63,54 +105,56 @@ Clay_RenderCommandArray build_layout(void)
   // Create layout
   Clay_BeginLayout();
 
-  CLAY(CLAY_ID("Main"), CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}), CLAY_RECTANGLE({.color = COLOR_DARK}))
+  CLAY(CLAY_ID("Main"), CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}), CLAY_RECTANGLE({.color = ColorBg}))
   {
     CLAY(CLAY_ID("Header"),
          CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_FIXED(50)}, .childAlignment = {CLAY_ALIGN_X_CENTER, CLAY_ALIGN_Y_CENTER}, .childGap = 16, .padding = {32}}),
-         CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+         CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
     {
-      CLAY_TEXT(CLAY_STRING("Mars Rover Control Panel"), CLAY_TEXT_CONFIG(headerTextConfig));
+      CLAY_TEXT(CLAY_STRING("Mars Rover Control Panel"), CLAY_TEXT_CONFIG(HeaderTextConfig));
     }
 
-    CLAY(CLAY_ID("CenterContainer"), CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, .childGap = 16}), CLAY_RECTANGLE({.color = COLOR_DARK}))
+    CLAY(CLAY_ID("CenterContainer"), CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, .childGap = 16}), CLAY_RECTANGLE({.color = ColorBg}))
     {
       CLAY(CLAY_ID("SideBar1"),
            CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_FIXED(300), .height = CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}),
-           CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+           CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
       {
         // When id's are button_1, button_2, button_3, button_4, they all seem to get the same hash?
         // Should investigate the exact output of the clay hashing function
         // assert(Clay__HashString(CLAY_STRING("button_1"), 0, 0) == Clay__HashString(CLAY_STRING("button_1"), 0, 0)); ??????
 
-        button_element("button_1", CLAY_STRING("Power On"));
-        button_element("button_2", CLAY_STRING("Power Thrusters"));
-        button_element("button_3", CLAY_STRING("Collect Samples"));
-        button_element("button_4", CLAY_STRING("Emergency Shutdown"));
+        button_element(PowerOnButtonID, CLAY_STRING("Power On"), power_on_button);
+        button_element(PowerThrustersButtonID, CLAY_STRING("Power Thrusters"), power_thrusters_button);
+        button_element(CollectSamplesButtonID, CLAY_STRING("Collect Samples"), collect_samples_button);
+        button_element(EmergencyShutdownButtonID, CLAY_STRING("Emergency Shutdown"), emergency_shutdown_button);
       }
 
       CLAY(CLAY_ID("MainContent"),
            CLAY_LAYOUT({.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_GROW()}}),
-           CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+           CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
       {
       }
 
       CLAY(CLAY_ID("SideBar2"),
            CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_FIXED(300), .height = CLAY_SIZING_GROW()}, .padding = {16, 16}, .childGap = 16}),
-           CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+           CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
       {
+        CLAY(CLAY_ID("SyntaxPageRightImage"), CLAY_LAYOUT({.sizing = {CLAY_SIZING_GROW()}, .childAlignment = {.x = CLAY_ALIGN_X_CENTER}}))
+        {
+          CLAY(CLAY_ID("SyntaxPageRightImageInner"), CLAY_IMAGE({.sourceDimensions = {360, 360}, .imageData = &testImage})) {}
+        }
       }
     }
 
     CLAY(CLAY_ID("Footer"),
          CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_FIXED(200)}, .childAlignment = {0, 0}, .childGap = 16, .padding = {32}}),
-         CLAY_RECTANGLE({.color = COLOR_LIGHT, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
+         CLAY_RECTANGLE({.color = ColorLight, .cornerRadius = {8.0f, 8.0f, 8.0f, 8.0f}}))
     {
-      CLAY_TEXT(CLAY_STRING(""), CLAY_TEXT_CONFIG(footerTextConfig));
-      CLAY_TEXT(CLAY_STRING("> Samples Collected by Rover"), CLAY_TEXT_CONFIG(footerTextConfig));
-      CLAY_TEXT(CLAY_STRING("> issued command: Power Thrusters"), CLAY_TEXT_CONFIG(footerTextConfig));
-      CLAY_TEXT(CLAY_STRING("> issued command: Power On"), CLAY_TEXT_CONFIG(footerTextConfig));
-      CLAY_TEXT(CLAY_STRING("> issued command: Power Off"), CLAY_TEXT_CONFIG(footerTextConfig));
-      CLAY_TEXT(CLAY_STRING("> issued command: Power On"), CLAY_TEXT_CONFIG(footerTextConfig));
+      CLAY_TEXT(CLAY_STRING(""), CLAY_TEXT_CONFIG(FooterTextConfig));
+      CLAY_TEXT(CLAY_STRING("> Samples Collected by Rover"), CLAY_TEXT_CONFIG(FooterTextConfig));
+      CLAY_TEXT(CLAY_STRING("> issued command: Power Thrusters"), CLAY_TEXT_CONFIG(FooterTextConfig));
+      CLAY_TEXT(CLAY_STRING("> issued command: Power On"), CLAY_TEXT_CONFIG(FooterTextConfig));
     }
   }
 
@@ -130,35 +174,35 @@ int main(void)
   Clay_Raylib_Initialize((int)ScreenWidth, (int)ScreenHeight, "Mars Rover Control Panel", FLAG_BORDERLESS_WINDOWED_MODE | FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
 
   // Init Clay
-  uint64_t clayMemorySize = Clay_MinMemorySize();
-  Clay_Arena memoryArena = {
-      .memory = malloc(clayMemorySize),
-      .capacity = clayMemorySize,
+  uint64_t clay_memory_size = Clay_MinMemorySize();
+  Clay_Arena memory_arena = {
+      .memory = malloc(clay_memory_size),
+      .capacity = clay_memory_size,
   };
   Clay_Dimensions dimensions = {
       .width = ScreenWidth,
       .height = ScreenHeight,
   };
   Clay_SetMeasureTextFunction(Raylib_MeasureText);
-  Clay_Initialize(memoryArena, dimensions);
+  Clay_Initialize(memory_arena, dimensions);
 
   // ...more init??
   SetTargetFPS(144);
 
-  insert_font(FONT_ID_NORMAL_16, 16, "resources/0xProto-Regular.ttf");
-  insert_font(FONT_ID_NORMAL_24, 24, "resources/0xProto-Regular.ttf");
-  insert_font(FONT_ID_BOLD_16, 16, "resources/0xProto-Bold.ttf");
-  insert_font(FONT_ID_BOLD_24, 24, "resources/0xProto-Bold.ttf");
-  insert_font(FONT_ID_ITALIC_16, 16, "resources/0xProto-Italic.ttf");
-  insert_font(FONT_ID_ITALIC_24, 24, "resources/0xProto-Italic.ttf");
-  insert_font(FONT_ID_TITLE, 36, "resources/0xProto-Regular.ttf");
+  insert_font(FontIdNormal16, 16, "resources/0xProto-Regular.ttf");
+  insert_font(FontIdNormal24, 24, "resources/0xProto-Regular.ttf");
+  insert_font(FontIdBold16, 16, "resources/0xProto-Bold.ttf");
+  insert_font(FontIdBold24, 24, "resources/0xProto-Bold.ttf");
+  insert_font(FontIdItalic16, 16, "resources/0xProto-Italic.ttf");
+  insert_font(FontIdItalic24, 24, "resources/0xProto-Italic.ttf");
+  insert_font(FontIdTitle, 36, "resources/0xProto-Regular.ttf");
+
+  testImage = LoadImage("resources/test.png");
 
   // Main application loop
   while (!WindowShouldClose())
   {
     float dt = GetFrameTime();
-
-    // Logic code
 
     // Update states
     Vector2 raylibMousePosition = GetMousePosition();
@@ -166,7 +210,7 @@ int main(void)
         .x = raylibMousePosition.x,
         .y = raylibMousePosition.y,
     };
-    Clay_SetPointerState(mousePosition, IsMouseButtonDown(MOUSE_LEFT_BUTTON));
+    Clay_SetPointerState(mousePosition, IsMouseButtonDown(MOUSE_BUTTON_LEFT));
 
     Vector2 raylibMouseWheelDelta = GetMouseWheelMoveV();
     Clay_Vector2 mouseWheelDelta = {
